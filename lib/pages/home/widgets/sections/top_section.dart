8c89da742dd8fc0bf34e0ad395ffcda2be06e523
thalesterra <nbthales@gmail.com>
2021-08-08 13:03:11 -0300

    28. Criando a Seção do Topo para Tablet

--- lib/pages/home/widgets/sections/top_section.dart
+++ lib/pages/home/widgets/sections/top_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_clone_web/breakpoints.dart';
 import 'package:udemy_clone_web/pages/home/widgets/custom_search_field.dart';
 
 class TopSection extends StatelessWidget {
@@ -42,6 +43,54 @@ class TopSection extends StatelessWidget {
                             'Bora aprender Flutter com o professor Daniel Ciolfi! Cursos por apenas R\$29,90. Qualidade garantida.',
                             style: TextStyle(
                               fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          CustomSearchField(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        if(maxWidth >= mobileBreakpoint){
+          return SizedBox(
+            height: 320,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.network('https://images.pexels.com/photos/892757/pexels-photo-892757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', fit: BoxFit.cover),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 20,
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 8,
+                    child: Container(
+                      padding: const EdgeInsets.all(22),
+                      width: 350,
+                      child: Column(
+                        children: [
+                          Text(
+                            'Aprenda Flutter com este curso',
+                            style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Bora aprender Flutter com o professor Daniel Ciolfi! Cursos por apenas R\$29,90. Qualidade garantida.',
+                            style: TextStyle(
+                              fontSize: 15,
                               color: Colors.white,
                             ),
                           ),
